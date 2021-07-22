0efd7463ff2a3059e19186222c14f7940b8a56ba
5410tiffany <37042357+5410tiffany@users.noreply.github.com>
2021-07-22 15:07:24 +0800

    Create sol.py

---
+++ 11. Container With Most Water/sol.py
@@ -0,0 +1,42 @@
+import math
+class Solution:
+    def maxArea(self, height: List[int]) :
+        '''
+        (height, idx) 
+        想法:
+        1. 算area只考慮height比自己大的 則area = heighti * abs(idxi - idxj)
+        2. abs(idxi - idxj) 最大值必出現在idxj = minidx or maxidx 的地方
+        time: O(n)
+        '''
+        ls = []
+        
+        # buid (height, idx) 2D list
+        for i in range(len(height)):
+            ls.append([height[i], i])
+        
+        # sort according to height
+        print(ls)
+        ls.sort()
+        print(ls)
+        
+        maxidx = ls[len(height)-1][1]
+        minidx = ls[len(height)-1][1]
+        maxarea = 0
+        
+        # iterate 
+        for i in range(len(height)-2, -1, -1):
+            
+            curidx = ls[i][1]
+            curheight = ls[i][0]
+            print(f'{maxarea}: H-{ls[i][0]}, L-({maxidx}, {minidx}, {curidx}, {max(abs(maxidx-curidx), abs(minidx-curidx))})')
+            
+            # update area
+            maxarea = max(maxarea, curheight * abs(maxidx-curidx), curheight * abs(minidx-curidx))
+            # update idx
+            maxidx = max(maxidx, ls[i][1])
+            minidx = min(minidx, ls[i][1])
+            
+        
+
+        print(maxarea)
+        return maxarea
