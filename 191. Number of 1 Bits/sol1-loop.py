9e28f0698d5e132244a3205c264af127f84c7425
5410tiffany <37042357+5410tiffany@users.noreply.github.com>
2021-08-05 16:20:55 +0800

    Update sol1-loop.py

--- 191. Number of 1 Bits/sol1-loop.py
+++ 191. Number of 1 Bits/sol1-loop.py
@@ -1,12 +1,19 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        bits = 0;
+        cnt = 0;
         mask = 1;
-        for i in range(32):
-            if ((n & mask) != 0):
-                bits+=1;
-            
-            mask <<= 1;
+    
+        '''
+        for each bit, get its number and check if it's 1 
+        using & operator and mask
         
-        return bits;
+        '''
+        for i in range(32): 
+            if ((n & mask) != 0):
+                cnt+=1;
+            
+            print(list(bin(mask)))
+            mask = mask << 1 # left shift bit
+        
+        return cnt;
     
