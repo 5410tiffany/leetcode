6d5468b1c6e2dbcd96261c1dfedf1cc2f4d3c394
5410tiffany <37042357+5410tiffany@users.noreply.github.com>
2021-08-29 22:35:14 -0400

    Create sol-recursion.py

---
+++ 1143. Longest Common Subsequence/sol-recursion.py
@@ -0,0 +1,34 @@
+class Solution:
+    def memo_solve(self, p1, p2): # 'a' is at idx p1 in text1
+        
+        # terminal pt if either subseq is empty
+        if p1 == len(self.text1) or p2 == len(self.text2): return 0
+        
+        
+        # no: 'a' not in optimal sol 
+        optnop1 = self.memo_solve(p1 + 1, p2)
+        
+        # yes: 'a' in optimal sol
+        p1intext2 = self.text2.find(self.text1[p1], p2) # find 'a' start from position p2
+        optyesp1 = 0
+        
+        if p1intext2 != -1: # 'a' is in p2 subseq
+            optyesp1 = self.memo_solve(p1 + 1, p1intext2 + 1) + 1
+        
+        return max(optnop1, optyesp1)
+    def longestCommonSubsequence(self, text1: str, text2: str):
+        '''
+        break down into subproblem: a 是不是在optimal sol中
+        1. no -> lcs(s1, s2) = lcs(s1-a, s2)
+        2. yes-> lcs(s1, s2) = lcs(s1-a, s2-a) + 1
+        '''
+        #debug 
+        self.text1 = text1
+        self.text2 = text2
+
+        
+        return self.memo_solve(0, 0)
+        
+        
+        
+        
